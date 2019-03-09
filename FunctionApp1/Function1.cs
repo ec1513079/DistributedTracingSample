57ace865ba7935e59b6c54327dd1ca177081cf77
Kotaro Takahashi <ec1513079@gmail.com>
2019-03-09 14:22:14 +0900

    add function2

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -1,5 +1,9 @@
 
+using System;
 using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -11,8 +15,29 @@ namespace FunctionApp1
 {
     public static class Function1
     {
+        static HttpClient httpClient = new HttpClient();
+
         [FunctionName("HttpFunction1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static async Task<IActionResult> HttpFunction1([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info($"HttpFunction1 C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+            
+            var response = await httpClient.PostAsync($"{req.Scheme}://{req.Host}/api/{nameof(HttpFunction2)}"
+                , new StringContent(JsonConvert.SerializeObject(new { name = name }), Encoding.UTF8, @"application/json"));
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {await response.Content.ReadAsStringAsync()}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+        
+        [FunctionName("HttpFunction2")]
+        public static async Task<IActionResult> HttpFunction2([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info($"HttpFunction1 C# HTTP trigger function processed a request.");
 
@@ -23,7 +48,7 @@ namespace FunctionApp1
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"‚±‚ñ‚É‚¿‚Í, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
